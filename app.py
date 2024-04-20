9108ad649bde9dd0763ea9eff3865aecb5d01aff
Kris Gospodinov <kris060601@gmail.com>
2024-04-20 13:43:16 +0300

    Create users and recipes tables sqlite3

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 from flask import Flask, render_template
+import sqlite3
 
+# Initialize the Flask application
 app = Flask(__name__)
+
+# Connect to the database
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+c.execute(""" 
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        hash TEXT NOT NULL
+    )""")
+c.execute("PRAGMA foreign_keys = ON")
+
+c.execute(""" 
+    CREATE TABLE IF NOT EXISTS recipes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        name TEXT NOT NULL,
+        description TEXT,
+        total_time INTEGER,
+        category TEXT,
+        instructions TEXT,
+        difficulty INTEGER,
+        rating INTEGER,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )""")
+# c.execute("""
+#     INSERT INTO recipes (name, description, total_time, category, instructions, difficulty, rating)
+#     VALUES (?, ?, ?, ?, ?, ?, ?)
+# """, ('amaa', 'lalal', 30, 'italian', 'cook amaa', 1, 5))
+
+# c.execute("SELECT * FROM recipes")
+# print(c.fetchall())
+
+conn.commit()
+conn.close()
 
 @app.route('/')
 def home():
